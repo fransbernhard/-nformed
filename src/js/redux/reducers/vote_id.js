b8bb8e5519f847f8f40e8e81d4c02abd3f3cb2c2
logic93 <jeanmelki1@gmail.com>
2018-06-12 10:37:23 +0200

    bugfix

--- src/js/redux/reducers/vote_id.js
+++ src/js/redux/reducers/vote_id.js
@@ -1,12 +1,9 @@
 import { SET_VOTE_ID } from "../action-types/index"
 import store from "../store/index"
 
-<<<<<<< HEAD
-=======
 /*
     Reducer for storing a vote id
 */
->>>>>>> cf63b9ef082ca8d06aa07931a2b359915994cd83
 const vote_id = (state = {}, action) => {
     switch (action.type) {
         case SET_VOTE_ID:
